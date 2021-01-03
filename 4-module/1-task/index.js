c862e37fa8116034b02639758bd64feb19bf80f7
Irina Nicolai <irina.nic.yu@gmail.com>
2021-01-03 21:47:44 +0200

    Added makeFriendsList function

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,18 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let friendsList = document.createElement('ul');
+
+  document.body.append(friendsList);
+
+  friends.forEach(({firstName: name, lastName: surname}) => {
+    let friend = document.createElement('li');
+    friend.innerHTML = `${name} ${surname}`;
+    
+    friendsList.append(friend);
+
+  });
+
+  return document.body.lastElementChild;
 }
+
