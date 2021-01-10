7f0e9166755d5dc658bb019db7a08987c7e25ce7
Irina Nicolai <irina.nic.yu@gmail.com>
2021-01-10 21:26:45 +0200

    Added toggleText function

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  let toggleButton = document.querySelector('.toggle-text-button'),
+    textContainer = document.querySelector('#text');
+
+  toggleButton.addEventListener('click', () => {
+
+    if (!textContainer.hasAttribute('hidden')) {
+      textContainer.setAttribute('hidden', null);
+    } else {
+      textContainer.removeAttribute('hidden');
+    }
+
+  });
 }
