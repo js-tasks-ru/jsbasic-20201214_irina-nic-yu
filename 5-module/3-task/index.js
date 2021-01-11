32b543f0b6065e06a2f54ac8661c345760027373
Irina Nicolai <irina.nic.yu@gmail.com>
2021-01-11 21:01:00 +0200

    Added initCarousel function

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  const slidesContainer = document.querySelector('.carousel__inner'),
+    carouselSlides = document.querySelectorAll('.carousel__slide'),
+    carouselArrow = document.querySelectorAll('.carousel__arrow'),
+    rightArrow = carouselArrow[0],
+    leftArrow = carouselArrow[1];
+
+  const slideWidth = carouselSlides[0].offsetWidth;
+
+  let actualSlidePosition = 0;
+  leftArrow.style.display = 'none';
+
+  rightArrow.addEventListener('click', () => {
+    leftArrow.style.display = '';
+    actualSlidePosition++;
+    slidesContainer.style.transform = `translateX(-${slideWidth*actualSlidePosition}px)`;
+
+    if (actualSlidePosition >= (carouselSlides.length - 1)) {
+      rightArrow.style.display = 'none';
+    }
+
+  });
+
+  leftArrow.addEventListener('click', () => {
+    rightArrow.style.display = '';
+    actualSlidePosition--;
+    slidesContainer.style.transform = `translateX(-${slideWidth*actualSlidePosition}px)`;
+
+    if (actualSlidePosition <= 0) {
+      leftArrow.style.display = 'none';
+    }
+
+  });
+
 }
