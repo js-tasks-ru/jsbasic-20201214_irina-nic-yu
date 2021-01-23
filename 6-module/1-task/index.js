8a267df05455848b9c00023497c99b10c9d06967
Irina Nicolai <irina.nic.yu@gmail.com>
2021-01-23 22:53:17 +0200

    Added Tasks 6.1, 6.2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,11 +5,11 @@
  * Пример одного элемента, описывающего строку таблицы
  *
  *      {
-     *          name: '',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *   },
+ *          name: '',
+ *          age: 25,
+ *          salary: '1000',
+ *          city: 'Petrozavodsk'
+ *   },
  *
  * @constructor
  */
@@ -28,7 +28,67 @@
  *
  * @constructor
  */
+import createElement from '../../assets/lib/create-element.js';
 export default class UserTable {
+
   constructor(rows) {
+    this._users = rows;
+    this._headings = ['Имя', 'Возраст', 'Зарплата', 'Город', ''];
+    this._container = null;
+    this._hideRow = this._hideRow.bind(this);
+    this._render();
   }
+
+  get elem() {
+    return this._container;
+  }
+
+  _tableTemplate({headings = [], rows = []} = {}) {
+    return `
+    <table data-component="hidingRowsTable">
+    <thead>
+    ${headings}
+    </thead>
+    <tbody>${rows}</tbody>
+    </table>`;
+  }
+
+  _rowTemplate(user) {
+    return `<tr>
+    <td>${user.name}</td>
+    <td>${user.age}</td>
+    <td>${user.salary}</td>
+    <td>${user.city}</td>
+    <td><button data-component='hideButton'>X</button></td>
+    </tr>`;
+  }
+
+  _createHeadings({headings = []} = {}) {
+    return `<tr>${headings.map((item) => `<th>${item}</th>`).join('')}</tr>`;
+  }
+  _createRows({rows = []} = {}) {
+    return rows.map(this._rowTemplate).join('');
+  }
+
+  _hideRow(event) {
+    let target = event.target;
+    let row = target.closest('tr');
+
+    if (target.dataset.component != 'hideButton') {
+      return;
+    }
+    row.remove();
+  }
+
+  _render() {
+    const template = this._tableTemplate({
+      headings: this._createHeadings({headings: this._headings}),
+      rows: this._createRows({rows: this._users})
+    });
+
+    this._container = createElement(template);
+    this._container.addEventListener('click', this._hideRow);
+  }
+
 }
+
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,66 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    this._name = product.name;
+    this._price = product.price;
+    this._category = product.category;
+    this._image = product.image;
+    this._id = product.id;
+    this._container = null;
+ 
+    this._render();
   }
+
+  get elem() {
+    return this._container;
+  }
+
+  _cardTemplate({image, price, name}) {
+    return `<div class="card"><div class="card">
+        <div class="card__top">
+            <img src="/assets/images/products/${image}" class="card__image" alt="product">
+            <span class="card__price">€${price}</span>
+        </div>
+        <div class="card__body">
+            <div class="card__title">${name}</div>
+            <button type="button" class="card__button">
+                <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+        </div>
+    </div></div>`;
+  }
+
+  _render() {
+    const template = this._cardTemplate({
+      image: this._image, 
+      price: this._price.toFixed(2), 
+      name: this._name
+    });
+
+    this._container = createElement(template);
+    this.addEventListeners();
+  }
+  
+  addEventListeners() {
+    this._container.addEventListener('click', this._onCardClick);
+    this._container.addEventListener('product-add', console.log);
+  }
+
+  _onCardClick = (event) => {
+    let target = event.target;
+
+    if (!target.closest(`[class="card__button"]`)) {
+      return;
+    }
+
+    const productAddEvent = new CustomEvent(
+      'product-add', {
+        detail: this._id,
+        bubbles: true
+      });
+      
+    this._container.dispatchEvent(productAddEvent);
+  }
+  
 }
+
